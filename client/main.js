52d373c4605d24b3a8cec1be402e9f1ad360d1e4
Adarsh Chourasia <adarshchourasia.in@gmail.com>
2021-11-21 10:51:44 +0530

    mail.js_part4

--- client/main.js
+++ client/main.js
@@ -54,6 +54,76 @@ function loadDropdownList(data) {
     drop2.innerHTML = dropdownHtml;
 }
 
+const submitButton = document.querySelector('#submit-btn');
+
+submitButton.onclick = function () {
+    const email1 = document.querySelector("#name-of-interviewer").value;
+    const email2 = document.querySelector("#name-of-student").value;
+    const startTime = document.querySelector("#start-time").value;
+    const endTime = document.querySelector("#end-time").value;
+    
+    //console.log(email1, email2, startTime,endTime);
+
+    if(email1 === email2) {
+        alert("Interviewer and Interviewee cannot be same");
+        return;
+    }
+    if(startTime === "" || endTime === "") {
+        alert("Select Date and Time");
+        return;
+    }
+    fetch('http://localhost:5000/insertInterview', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({ email1: email1,
+                               email2 : email2,
+                               startTime : startTime,
+                               endTime : endTime})
+    })
+    .then(response => response.json())
+    .then(data => insertRowIntoInterviewTable(data['data']));
+}
+
+function insertRowIntoInterviewTable(data) {
+
+    if(data.id === -1) {
+        alert("Interviewer Not available at that time");
+        return;
+    }
+    if(data.id === -2) {
+        alert("Interviewee Not available at that time");
+        return;
+    }
+    const table = document.querySelector('table tbody');
+    const isTableData = table.querySelector('.no-data');
+
+    let tableHtml = "<tr>";
+
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            if (key === 'startTime' || key==='endTime') {
+                data[key] = new Date(data[key]).toLocaleString();
+            }
+            tableHtml += `<td>${data[key]}</td>`;
+        }
+    }
+    const dataToStore = `${data.id},${data.email1},${data.email2}`;
+    tableHtml += `<td><button class="delete-row-btn" data-id=${data.id}>Delete</td>`;
+    tableHtml += `<td><button class="edit-row-btn" data-id=${dataToStore}>Edit</td>`;
+
+    tableHtml += "</tr>";
+
+    if (isTableData) {
+        table.innerHTML = tableHtml;
+    } else {
+        const newRow = table.insertRow();
+        newRow.innerHTML = tableHtml;
+    }
+}
+
+
 function loadInterviewTable(data) {
     const table = document.querySelector('table tbody');
 
@@ -109,4 +179,16 @@ updateBtn.onclick = function() {
     })
     .then(response => response.json())
     .then(data => updateVerdict(data['data']));
+}
+
+function updateVerdict(data) {
+    if (data.id===-1) {
+        alert("Interviewer Not available at that time");
+    } 
+    else if (data.id===-2) {
+        alert("Interviewee Not available at that time");
+    }
+    else {
+        location.reload();
+    }
 }
